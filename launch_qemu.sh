8c2f0a61f741b7db6dcc1c545c8c367e18d541e0
Bill Hass <billhass@umich.edu>
2020-03-15 00:47:25 -0400

    Initial commit. Add launch_qemu.sh

---
+++ launch_qemu.sh
@@ -0,0 +1,23 @@
+STARTUP_RAM=512
+NUM_CPUS=2
+GUEST_IP=10.0.2.10
+LINUX_KERNEL_SOURCE_PATH=linux_kernels/linux-4.16.1/
+KERNEL_ZIMAGE_PATH="$LINUX_KERNEL_SOURCE_PATH"arch/arm/boot/zImage
+DTB_PATH="$LINUX_KERNEL_SOURCE_PATH"arch/arm/boot/dts/vexpress-v2p-ca15-tc1.dtb
+
+
+BUILDROOT_PATH=buildroot/
+LINUX_ROOTFS_PATH="$BUILDROOT_PATH"output/images/rootfs.ext2
+
+qemu-system-arm \
+	-m $STARTUP_RAM \
+	-smp $NUM_CPUS \
+	-net nic -net user,host=$GUEST_IP,hostfwd=tcp::10022-:22 \
+	-display none \
+	-serial stdio \
+	-machine vexpress-a15 \
+	-dtb $DTB_PATH \
+	-sd "$LINUX_ROOTFS_PATH" \
+	-snapshot \
+	-kernel $KERNEL_ZIMAGE_PATH \
+	-append "earlyprintk=serial console=ttyAMA0 root=/dev/mmcblk0"
